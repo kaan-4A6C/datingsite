4c9c45a6f37afc77cf58d6ae8110854ce791b662
kaan-4A6C <510507@edu.rocmn.nl>
2017-10-03 16:00:31 +0200

    hoplijk werkt het

--- review/lexa.php
+++ review/lexa.php
@@ -210,7 +210,7 @@
     <h2 class="text-center">Facebook comments</h2>
     <div class="text-center">
         <?php
-            $db = new mysqli("localhost", "root", "", "dating_site");
+            $db = new mysqli("46.105.68.243", "Ojo07y8malf_MrvM", "flirtjessgidssen", "Ojo07y8malf_MrvM");
 
             $query = "SELECT AVG(rating) / 10 as 'avg' FROM reviews WHERE dating_site_id = 1";
 
@@ -220,36 +220,36 @@
         ?>
     </div>
     <?php
-            $stars =  '<div class="row flex">
-                    <fieldset class="rating">
-                         <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-                         <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-                         <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-                         <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-                         <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
-                         <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-                         <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-                         <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-                         <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-                         <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-                    </fieldset>
+        $stars =  '<div class="row flex">
+                <fieldset class="rating">
+                     <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                     <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                     <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                     <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                     <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                     <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                     <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                     <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                     <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                     <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                </fieldset>
 
-                 </div>';
+             </div>';
 
-                 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                     $ip = $_SERVER['HTTP_CLIENT_IP'];
-                 } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-                 } else {
-                     $ip = $_SERVER['REMOTE_ADDR'];
-                 }
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else {
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
 
-                 $query = "SELECT * FROM reviews WHERE dating_site_id = 1 AND ip = '$ip'";
-                 $result = $db->query($query);
-                 if ($result->num_rows === 0) {
-                    echo $stars;
-                 }
-             ?>
+            $query = "SELECT * FROM reviews WHERE dating_site_id = 1 AND ip = '$ip'";
+            $result = $db->query($query);
+            if ($result->num_rows === 0) {
+                echo $stars;
+            }
+        ?>
       <hr>
       <div class="panel panel-primary" style="position:relative">
         <div class="panel-heading">Facebook comments</div>
